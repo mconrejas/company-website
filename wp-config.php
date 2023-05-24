c3f3394af6cf56019db3e359a4d7fa8b3a659174
checcoy <napalitcherry@gmail.com>
2023-05-24 15:42:36 +0800

    09/24/2023

--- wp-config.php
+++ wp-config.php
@@ -92,5 +92,4 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 /** Sets up WordPress vars and included files. */
 require_once ABSPATH . 'wp-settings.php';
-define('WP HOME','http://company-website.test/');
-define('WP SITEURL','http://company-website.test/');
+
